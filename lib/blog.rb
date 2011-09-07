617915edf63106c2538e7daffc6999977d6c50ed
Shane Tjong <shanetj@gmail.com>
2011-09-07 17:34:25 -0400

    Trying to get the latest title from the blog

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,3 +1,11 @@
-#
-# Create your "Blog" class in this file.
-#
+class Blog
+  def initialize url 
+    @url = url
+  end
+  
+  def latest_title
+    thewebsite = Net::HTTP.get(URI.parse(@url))
+    document = Hpricot(thewebsite)
+(document/"/html/body/div[2]/div/div[2]/div/div/h3").first.inner_html
+  end
+end
